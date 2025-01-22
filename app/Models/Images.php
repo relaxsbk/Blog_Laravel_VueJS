90c2cd790a5f7d6b4b7d48b9715024c172330177
relaxsbk.exe <pah4n1989@gmail.com>
2025-01-22 18:13:54 +0800

    Создание миграции и модели картинок

---
+++ app/Models/Images.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Images extends Model
+{
+    protected $table = 'images';
+
+    protected $fillable = [
+        'path',
+        'url'
+    ];
+}
